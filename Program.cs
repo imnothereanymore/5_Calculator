66d589fdcd8fcaedd2852421d9e865a563f120e9
imnothereanymore <125987867+imnothereanymore@users.noreply.github.com>
2025-02-12 13:47:32 +0500

    33

--- Program.cs
+++ Program.cs
@@ -1,73 +1,71 @@
 using OpenTelemetry.Metrics;
 using static System.Net.WebRequestMethods;
 
-namespace _5_Calculator
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        meterProviderBuilder.AddPrometheusExporter();
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+            "Microsoft.AspNetCore.Server.Kestrel");
 
-            builder.Services.AddOpenTelemetry()
-                .WithMetrics(meterProviderBuilder =>
-                {
-                    meterProviderBuilder.AddPrometheusExporter();
-
-                    meterProviderBuilder.AddMeter(
-                        "Microsoft.AspNetCore.Hosting",
-               "Microsoft.AspNetCore.Server.Kestrel");
-
-                    // Status code 
-                    meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+        // Status code 
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
 
 
-                    meterProviderBuilder.AddView("http.server.request.duration",
-                        new ExplicitBucketHistogramConfiguration
-                        {
-                            Boundaries = [
-                                0,
-                                0.005,
-                                0.01,
-                                0.025,
-                                0.05,
-                                0.075,
-                                0.1,
-                                0.25,
-                                0.5,
-                                0.75,
-                                1,
-                                2.5,
-                                5,
-                                7.5,
-                                10
-                            ]
-                        });
-                });
-
-            var app = builder.Build();
-
-            app.MapPrometheusScrapingEndpoint();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+        meterProviderBuilder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
             {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
+                Boundaries =
+                [
+                    0,
+                    0.005,
+                    0.01,
+                    0.025,
+                    0.05,
+                    0.075,
+                    0.1,
+                    0.25,
+                    0.5,
+                    0.75,
+                    1,
+                    2.5,
+                    5,
+                    7.5,
+                    10
+                ]
+            });
+    });
 
-            app.UseRouting();
 
-            app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Calculator}/{action=Index}/{id?}");
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
-            app.Run();
-        }
-    }
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Calculator/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapPrometheusScrapingEndpoint();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Calculator}/{action=Index}/{id?}");
+
+app.Run();
